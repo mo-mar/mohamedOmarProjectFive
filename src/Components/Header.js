cb99ca0e150cc3342a9c22e0f00acbd0f44a39c7
Mohamed Omar <mohamedsomar1@gmail.com>
2019-09-05 12:55:35 -0400

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Partials/header.scss'
+
+const Header = () =>{
+    return(
+        <header>
+            <div class="wrapper">
+                <h1>Who's My Rep?</h1>
+                <h2>Wondering who the heck your political representatives are?</h2>
+                <p>This is the tool for you. Just enter your postal code below.</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
