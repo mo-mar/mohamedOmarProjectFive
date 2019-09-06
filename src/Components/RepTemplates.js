6daf9bdd31dffb868bdbba6dcfe37e676fe52a29
Mohamed Omar <mohamedsomar1@gmail.com>
2019-09-06 17:02:04 -0400

    added more conditional rendering operators

--- src/Components/RepTemplates.js
+++ src/Components/RepTemplates.js
@@ -6,18 +6,23 @@ class RepTemplates extends Component {
         const { arrayData } = this.props;
         const repData = arrayData.map((oneRep, index) => {
             return(
+
             <div className="repGroup" key={index}>
                 <h3 className="repName">{oneRep.name} is your {oneRep.elected_office} in the {oneRep.representative_set_name}</h3>
                 <div className="repFlex">
-                    <div className="repImageContainer">
-                        <img src={oneRep.photo_url} alt="A photograph of the political representative."/>
+                {oneRep.photo_url !== '' ? <div className="repImageContainer">
+                        <img src={oneRep.photo_url} alt="A photograph of the political representative." /> 
+                        </div> : 
+                        <div className="repImageContainer">
+                        <p class="imageErrorMessage">Oops! There's no photo available for this representative. Here's a nice alternative:</p>
+                        <img src="https://placedog.net/640/480?random" alt="A photograph of a cute dog."/>
+                        </div> }
+                    <div className="repInfo">
+                        <p>Learn more about {oneRep.first_name} <a href={oneRep.personal_url} target="_blank">here.</a></p>
+                        <p>Contact: <span className="email">{oneRep.email}</span>
+                        </p>
+                        {/* <p>Twitter: {on}</p> */}
                     </div>
-                        <div className="repInfo">
-                            <p>Learn more about {oneRep.first_name} <a href={oneRep.personal_url} target="_blank">here.</a></p>
-                            <p>Contact: <span className="email">{oneRep.email}</span>
-                            </p>
-                            <p>Twitter: {oneRep.extra.twitter}</p>
-                        </div>
                 </div>
             </div>
             )
