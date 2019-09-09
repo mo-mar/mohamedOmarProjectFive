738a8ae3eac2d215171ebe2ddb0bc1d08896ce58
Mohamed Omar <mohamedsomar1@gmail.com>
2019-09-09 19:13:19 -0400

    Updates

--- precache-manifest.372965ba1f600662b4618e8fc0bb7fd7.js
+++ precache-manifest.372965ba1f600662b4618e8fc0bb7fd7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "786c290e4dd2d62fdd69e165174d9339",
+    "revision": "c8e81901205494792382801bad8d2d51",
     "url": "/mohamedOmarProjectFive/index.html"
   },
   {
-    "revision": "a085d5e935f5ec084150",
-    "url": "/mohamedOmarProjectFive/static/css/main.a53c45ba.chunk.css"
+    "revision": "00ee0037200c2d2913eb",
+    "url": "/mohamedOmarProjectFive/static/css/main.4961bf55.chunk.css"
   },
   {
     "revision": "88030f64a045dc95ec92",
     "url": "/mohamedOmarProjectFive/static/js/2.6e28e343.chunk.js"
   },
   {
-    "revision": "a085d5e935f5ec084150",
-    "url": "/mohamedOmarProjectFive/static/js/main.33851be9.chunk.js"
+    "revision": "00ee0037200c2d2913eb",
+    "url": "/mohamedOmarProjectFive/static/js/main.7cc438fc.chunk.js"
   },
   {
     "revision": "ef76fa75b1db5ceef330",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mohamedOmarProjectFive/precache-manifest.8c7b69285af2b81cb938974c3f35bef0.js"
+  "/mohamedOmarProjectFive/precache-manifest.372965ba1f600662b4618e8fc0bb7fd7.js"
 );
 
 self.addEventListener('message', (event) => {
