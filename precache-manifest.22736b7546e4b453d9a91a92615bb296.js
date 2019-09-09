8e5386d3fb14a901ff7275ea63af2afd0d7fc83c
Mohamed Omar <mohamedsomar1@gmail.com>
2019-09-09 16:06:36 -0400

    Updates

---
+++ precache-manifest.22736b7546e4b453d9a91a92615bb296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9e721abe0729de2689f08c3f2d8a60",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "68c5027e54200bc593a9",
+    "url": "/my-app/static/css/main.465297ed.chunk.css"
+  },
+  {
+    "revision": "247a7cc4e2fd0c9c782f",
+    "url": "/my-app/static/js/2.a3045d02.chunk.js"
+  },
+  {
+    "revision": "68c5027e54200bc593a9",
+    "url": "/my-app/static/js/main.a79a85dd.chunk.js"
+  },
+  {
+    "revision": "645158e32e319827a444",
+    "url": "/my-app/static/js/runtime~main.69a61ae3.js"
+  }
+]);
